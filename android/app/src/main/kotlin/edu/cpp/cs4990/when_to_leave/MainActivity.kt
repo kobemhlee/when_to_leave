585e96fdf4a5bfb91c082035fa7ee7966f0ae049
kobemhlee <kobemhlee@gmail.com>
2020-02-17 22:25:56 -0800

    Initial commit

---
+++ android/app/src/main/kotlin/edu/cpp/cs4990/when_to_leave/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.cpp.cs4990.when_to_leave
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
